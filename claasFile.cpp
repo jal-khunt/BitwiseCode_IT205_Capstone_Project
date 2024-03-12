dc42fe9ae9d10a6c8370b1a1d24526db812af9e9
Jenish <jk1176vadodariya@gmail.com>
2024-03-12 16:20:35 +0530

    starting basic code

---
+++ claasFile.cpp
@@ -0,0 +1,8 @@
+class File{
+    public:
+
+    string path;
+    string dateModified;
+    int noOfTimeOpened;
+
+};
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include "claasFile.cpp"
+using namespace std;
+
+int main(){
+    vector<File> v;
+
+    
+    return 0;
+}
