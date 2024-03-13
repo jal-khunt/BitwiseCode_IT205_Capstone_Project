4c92a5a39593c5c6dee094bbe23e776ac412d849
Jenish <jk1176vadodariya@gmail.com>
2024-03-13 18:24:32 +0530

    update1

---
+++ inputFile.cpp
@@ -0,0 +1,32 @@
+#include<vector> 
+#include<string>
+#include<fstream>
+class File;
+using namespace std;
+vector<File> inputFile(string input){
+
+    string pathOfFile,lastModifiedDate;
+    int accessedNumber;
+    vector<File> v;
+    ifstream MyReadFile(input);
+    int i = 0;
+
+    while (MyReadFile.eof()==0) {
+        
+        File f;
+        v.push_back(f);
+        MyReadFile >> pathOfFile;
+        v[i].path = pathOfFile;
+
+
+        MyReadFile >> lastModifiedDate;
+        v[i].dateModified = lastModifiedDate;
+
+        MyReadFile >> accessedNumber;
+        v[i].noOfTimeOpened = accessedNumber;
+        i++;
+    }
+    MyReadFile.close();
+    return v;
+
+}
--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include<bits/stdc++.h>
+
+#include <fstream>
+#include <string>
 #include "claasFile.cpp"
+#include "inputFile.cpp"
+
 using namespace std;
 
 int main(){
-    vector<File> v;
 
+    vector<File> v = inputFile("input.txt");
     
+    cout << v[0].noOfTimeOpened << endl;
+
+
+    // Close the file
     return 0;
 }
