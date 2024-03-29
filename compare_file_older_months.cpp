21a09d591e0890bde356fede63afa0d58bf77d07
jal-khunt <160767599+jal-khunt@users.noreply.github.com>
2024-03-29 13:04:23 +0530

    for checking the condition for older months
    this function compare that  the file is older than user given months  if it is then it return true value otherwise return false.
    For second part of the problem Files old for more than N number of months.

---
+++ compare_file_older_months.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <sstream>
+// #include "classFile.cpp"
+
+class File;
+
+using namespace std;
+
+bool compare_months(string, vector<File> v, int i, int month)
+{
+    time_t tt;
+    struct tm* ti;
+    
+    time(&tt);
+    
+    ti = localtime(&tt);
+
+    int current_year = ti->tm_year + 1900, current_month = ti->tm_mon + 1;
+
+    stringstream ss(v[i].dateModified);
+
+    int file_year, file_month, file_day;
+    char discard;
+
+    ss >> file_day >> discard >> file_month >> discard >> file_year;
+
+    int months_diff;
+    if (current_month < file_month) {
+        months_diff = (current_year - file_year - 1) * 12 + (current_month + 12 - file_month);
+    } else {
+        months_diff = (current_year - file_year) * 12 + (current_month - file_month);
+    }
+
+    if(months_diff >= month){
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    
+}
