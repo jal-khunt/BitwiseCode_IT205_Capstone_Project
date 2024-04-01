a1422a9850e825b9941c0b9ff4db148f17376b52
mananghoia <mananghoia@gmail.com>
2024-04-01 18:26:54 +0530

    compare and delete function

--- comparation.cpp
+++ comparation.cpp
@@ -3,10 +3,13 @@
 #include <string>
 #include <vector>
 #include "classFile.cpp" // Include the header file or definition of File class
-#include "delete.cpp"
-#include "inputFile.cpp"
 
 using namespace std;
+
+// Function to compare text files
+bool compare_text(const string& text1, const string& text2) {
+    return text1 == text2;
+}
 
 // Function to compare files
 bool compareFiles(const string& filePath1, const string& filePath2) {
@@ -18,37 +21,58 @@ bool compareFiles(const string& filePath1, const string& filePath2) {
         return false;
     }
 
-    string line1, line2;
+    string content1((istreambuf_iterator<char>(file1)), istreambuf_iterator<char>());
+    string content2((istreambuf_iterator<char>(file2)), istreambuf_iterator<char>());
 
-    while (getline(file1, line1) && getline(file2, line2)) {
-        if (line1 != line2) {
-            return false;
-        }
+    return compare_text(content1, content2);
+}
+
+// Function to delete file
+void delete_file(const string& file_to_be_deleted, vector<File>& files, size_t index) {
+    char f[file_to_be_deleted.size() + 1];
+    f[file_to_be_deleted.size()] = '\0';
+    for (size_t i = 0; i < file_to_be_deleted.size(); i++) {
+        f[i] = file_to_be_deleted[i];
     }
 
-    return (!file1.eof() || !file2.eof());
-}
+    ifstream del(file_to_be_deleted);
 
-/*
-void compareOneToAnother(string input,int i, vector<File> v){
-    bool allFilesEqual = true;
-        for(int j = i + 1;j < v.size(); j++){
-            if (!compareFiles(v[i].path, v[j].path)) {
-                cout << "Files are not equal." << endl;
-                allFilesEqual = false;
-            } else {
-                cout << "Files are equal." << endl;
-                delete_file(v[j].path, input);
-            }
+    if (del.is_open()) {
+        del.close();
+
+        if (index < files.size()) {
+            files.erase(files.begin() + index);
+        } else {
+            cout << "Invalid index" << endl;
+            return;
         }
+        remove(f);
+    } else {
+        cout << "File doesn't exist\n";
+    }
 }
-*/
 
+// Function to read input file and create vector of File objects
+vector<File> inputFile(const string& input) {
+    string pathOfFile, lastModifiedDate;
+    int accessedNumber;
+    vector<File> v;
+    ifstream MyReadFile(input);
 
-int main() {
-    string inputFilePath = "input.txt"; // Path to input file
-    vector<File> files = inputFile(inputFilePath); // Read input file and get vector of File objects
+    while (MyReadFile >> pathOfFile >> lastModifiedDate >> accessedNumber) {
+        File f;
+        f.path = pathOfFile;
+        f.dateModified = lastModifiedDate;
+        f.noOfTimeOpened = accessedNumber;
+        v.push_back(f);
+    }
 
+    MyReadFile.close();
+    return v;
+}
+
+// Function to compare files and delete duplicates
+void compareAndDelete(vector<File>& files) {
     bool allFilesEqual = true;
 
     for (size_t i = 0; i < files.size(); ++i) {
@@ -60,7 +84,7 @@ int main() {
             } else {
                 cout << "Files are equal." << endl;
                 // Delete one of the files
-                delete_file(files[j].path, files);
+                delete_file(files[j].path, files, j); // Pass the index of the file to be deleted
             }
         }
     }
@@ -70,6 +94,13 @@ int main() {
     } else {
         cout << "Some files are not equal." << endl;
     }
+}
+
+int main() {
+    string inputFilePath = "input.txt"; // Path to input file
+    vector<File> files = inputFile(inputFilePath); // Read input file and get vector of File objects
+
+    compareAndDelete(files);
 
     return 0;
 }
