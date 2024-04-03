4704f1cc6aba1e8a9380061d4937a1d3cd079ae0
Shreyas Dutta <202301246@daiict.ac.in>
2024-04-03 19:20:41 +0530

    CSV support added finally

--- main.cpp
+++ main.cpp
@@ -42,7 +42,13 @@ int main(){
 
     //vector v is set;
 
-    for(int i = 0; i < v.size(); i++) cout << v[i].path << endl;
+    for(int i = 0; i < v.size(); i++) cout << v[i].path << " " << v[i].dateModified << v[i].noOfTimeOpened << endl;
+
+    //cout << v.size() << endl;
+
+    //delete_file(v[0].path, 0, v);
+
+    //cout << v.size()<<endl;
 
 
     
