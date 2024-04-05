0356ee8ce0f92c90b6cc7687255d565aeb025068
Shreyas Dutta <202301246@daiict.ac.in>
2024-04-05 23:28:46 +0530

    //

--- compare_months.cpp
+++ compare_months.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include "classDate.cpp"
 #include "delete.cpp"
@@ -7,38 +8,44 @@
 class File;
 using namespace std;
 
-
-bool compare_different(int i, vector<File> v, int month, string todaysDate){
+/*
+bool compare_different(int i, vector<File> &v, int month, string todaysDate)
+{
     Date currDate(todaysDate);
     Date fileDate(v[i].dateModified);
 
-    int months_diff = 0;
+    int months_diff;
 
     if (currDate.year == fileDate.year) {
 
         if (currDate.mounth == fileDate.mounth) {
-            return(currDate.date > fileDate.date);
-        }
-        else{
-            return (currDate.mounth > fileDate.mounth);
+            months_diff = 0;
+            months_diff += (currDate.date - fileDate.date) > 0 ? 1 : 0;
+            months_diff = currDate.mounth - fileDate.mounth - 1;
+            months_diff += (currDate.date >= fileDate.date) ? 1 : 0; 
         }
     } 
     else {
-        return(currDate.year > fileDate.year);
-    }
-        
-}
+        months_diff = (currDate.year - fileDate.year - 1) * 12;
+        months_diff += (12 - fileDate.mounth) + currDate.mounth;
+        months_diff += (currDate.date >= fileDate.date) ? 1 : 0; 
 
-int compare_months(vector<File> &v, int month, string today){
+    }
+    cout << months_diff << endl;
+
+    if(months_diff >= month)
+        return true;
+
+    return false;
+}
+*/
+
+int compare_months(vector<File> &v, int month, string date){
     int count = 0;
     for(int i = 0; i < v.size(); i++){
-        if(compare_different(i, v, month, today)){
-            int p = delete_file(v[i].path, i, v);
-            if(p == 0) continue;
-            count++;
-            i--;
+        if(compare_different(i, v, month, date)){
+            cout << endl << v[i].path << " ";
         }
     }
     return count;
-
 }
