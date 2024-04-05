d6c8ba81981d25d02ac08e8b2ab01b67354aa7d0
Shreyas Dutta <202301246@daiict.ac.in>
2024-04-05 13:50:22 +0530

    //

--- compare_months.cpp
+++ compare_months.cpp
@@ -7,6 +7,5 @@ using namespace std;
 
 bool compare_months_diffrent(int i, vector<File> v, int month, string todaysDate)
 {
-    Date currDate(todaysDate);
-    Date fileDate(v[i].dateModified);
+
 }
